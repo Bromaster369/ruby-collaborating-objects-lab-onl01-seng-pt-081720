b4082089b33ce465872ab0235741b3adecab769f
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-16 15:35:08 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,7 +25,22 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end 
   
-  def self.find_or_create_by_name 
-    
+  def self.find_or_create_by_name(name)
+   self.find(name) ? self.find(name) : self.new(name)
+  end 
+  
+  def self.find(name)
+    self.all.find {|artist| artist.name == name}
+  end 
+  
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end 
+  
+  def print_songs
+    songs.each {|song| puts song.name}
   end 
 end 
+
--- lib/song.rb
+++ lib/song.rb
@@ -17,11 +17,16 @@ class Song
   def save 
     @@all << self 
   end 
-  def self.new_by_filename
-    
+  
+  def self.new_by_filename(filename)
+    song_details = filename.chomp(".mp3").split(" - ")
+    song = Song.new(song_details[1])
+    song.artist_name = song_details[0]
+    song
   end 
   
-  def artist_name(name)
-    
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end 
 end 
