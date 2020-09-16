601a332b4aa25b81bcaa10abc66a86873c34158c
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-16 15:07:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,7 +26,6 @@ class Artist
   end 
   
   def self.find_or_create_by_name(name)
-   self.find(name) ? self.find(name) : self 
-    
+   self.find(name) ? self.find(name) : self.new(name)
   end 
 end 
