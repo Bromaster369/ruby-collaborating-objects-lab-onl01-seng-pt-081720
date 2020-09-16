0c223ada9a87e163f7bb1a7003326b5a917eacd6
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-16 14:23:17 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def add_song
+    
+  end 
+  
+  def songs 
+    
+  end 
+  
+  def self.find_or_create_by_name 
+    
+  end 
+end 
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+class MP3Importer 
+  
+  def initialize
+    
+  end 
+  
+  def files 
+    
+  end 
+  
+  def import 
+    
+  end 
+  
+end 
